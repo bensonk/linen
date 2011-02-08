a6fc963ee42e80ef65f8af8a7998a4b24ee0b7ab
Benson Kalahar <bensonk42@gmail.com>
2011-02-08 00:37:19 -0800

    Made it work for a subset of textile. Yay!

--- linen.js
+++ linen.js
@@ -1,136 +1,255 @@
-function linen(input) {
-  function handle_block_operations(block) {
-    function handle_attributes(block) {
-      // TODO
-      // (class)
-      // (#id)
-      // {style}
-      // [language]
-  
-      // TODO: do something
-      return block;
-    }
-  
-    function handle_alignment(block) {
-      // TODO
-      // > right
-      // < left
-      // = center
-      // <> justify
-  
-      // TODO: do something
-      return block;
+function lex(doc) {
+  function lex_block(block) {
+    var res = [];
+
+    // First, we look for a block descriptor.
+    var i = 0, c = block[0];
+    // TODO: Implement footnotes here. 
+    // TODO: Implement lists here. 
+    if(c == 'b') { res.push('b' + block[++i]) }
+    if(c == "h") { res.push('h' + block[++i]) }
+    if(c == "p") {
+      // TODO: Figure out why this is so broken
+      if(block.slice(i, i+3) == 'pre') {
+        res.push('pre');
+        i += 2;
+      }
+      else {
+        res.push("p");
+      }
     }
 
-    function handle_extended_blocks(block) {
-      // TODO Handle extended blocks
-      // p..
-      // bq..
-      // bc..
-      // pre..
-      return block;
+    for(i++; i < block.length; i++) {
+      var c = block[i];
+
+      // look for Alignment indicators
+      if(c == '>') { res.push('>') }
+      else if(c == '=') { res.push('=') }
+      else if(c == '<') {
+        // This is a special case, because the <> pair is considered an atom
+        if(block[i+1] == '>') {
+          i++;
+          res.push('<>');
+        }
+        else {
+          res.push('<');
+        }
+      }
+
+      // Look for classes & ids
+      else if(c == '(') {
+        var start = i;
+        while(block[i++] !== ')');
+        res.push(block.slice(start, i--));
+      }
+
+      // Look for languages
+      else if(c == '[') {
+        var start = i;
+        while(block[i++] !== ']');
+        res.push(block.slice(start, i--));
+      }
+
+      // look for styles
+      else if(c == '{') {
+        var start = i;
+        while(block[i++] !== '}');
+        res.push(block.slice(start, i--));
+      }
+
+      // look for our terminating character. It indicates the end of what this
+      // function is designed to lex. Here, we'll return our result, and this
+      // function can die happy.
+      else if(c == '.') {
+        return [ res, block.slice(++i, block.length).trim() ]
+          break;
+      }
+
+      // This implies a sort of parse error.  Sadly, my understanding of the
+      // textile grammar leads to unholy mixing of lexing and parsing, which is
+      // awfully messy, but here we are. 
+      else {
+        res = [ [ "p" ], block.trim() ];
+        break;
+      }
     }
-
-    // TODO: Actual Blocks
-    // hn. heading
-    // bq. Blockquote
-    // fnn. Footnote
-    // p. Paragraph
-    // bc. Block code
-    // pre. Pre-formatted
-
-    // TODO: Lists
-    // # Numeric list
-    // * Bulleted list
-
-    return block;
+    return res;
   }
 
-
-  function handle_images(block) {
-    // TODO
-    // !http://example.com/image.jpg! 
-
-    // TODO: do something
-    return block;
+  var blocks = doc.split("\n\n");
+  var result = [];
+  for(var i in blocks) {
+    result.push(lex_block(blocks[i]));
   }
 
-  function handle_acronyms(block) {
-    // TODO
-    // ABC(Always Be Closing)
+  return result;
+}
 
-    // TODO: do something
-    return block;
-  }
-
-  function handle_raw_html(block) {
-    // TODO
-    // ==No Textile Markup Here
-
-    // TODO: do something
-    return block;
-  }
-
-  function handle_tables(block) {
-    // TODO
-    // |_. a|_. table|_. header|
-    // |a|table|row|
-    // |a|table|row|
-
-    // TODO: do something
-    return block;
-  }
-
-  function handle_phrase_modifiers(block) {
-    // TODO
-    var modifiers = {
-      "_": "emphasis", 
-      "*": "strong", 
-      "__": "italic", 
-      "**": "bold", 
-      "??": "citation", 
-      "-": "deleted text", 
-      "+": "inserted text", 
-      "^": "superscript", 
-      "~": "subscript", 
-      "%": "span", 
-      "@": "code"
+function parse(doc) {
+  function parse_block(block, content) {
+    var obj = {
+      original_data: block,
+      block_type: block.shift(),
+      content: content,
+      classes: "",
+      id: "",
+      lang: "",
+      style: "",
+      alignment: ""
     };
+    for(var i in block) {
+      var atom = block[i];
 
-    // TODO: do something
-    return block;
+      // Look for classes and/or an id
+      if(atom[0] == '(') {
+        // Remove parens and split on #, for a quick and dirty parse
+        var parts = atom.replace(/^\(/, "").replace(/\)$/, "").split("#");
+
+        // Add to classes
+        if(obj.classes == "")
+          obj.classes = parts[0];
+        else
+          obj.classes = obj.classes + " " + parts[0];
+
+        // Set ID if it hasn't already been set
+        if(parts.length > 1 && obj.id == "")
+          obj.id = parts[1];
+      }
+
+      // Look for a language
+      else if(atom[0] == '[') {
+        obj.lang = atom.replace(/^\[/, "").replace(/\]$/, "");
+      }
+
+      // Look for a style
+      else if(atom[0] == '{') {
+        obj.style = atom.replace(/^\{/, "").replace(/\}$/, "");
+      }
+
+      // Look for alignment
+      else {
+        // Shortcut out if we already have an alignment set
+        if(obj.alignment == "") {
+          if(atom == '>') obj.alignment = "right";
+          if(atom == '<') obj.alignment = "left";
+          if(atom == '=') obj.alignment = "center";
+          if(atom == '<>') obj.alignment = "justify";
+        }
+      }
+    }
+
+    return obj;
   }
 
-  function handle_substitutions(block) {
-    var substitutions = {
-      // "quotes" → “quotes”
-      // 'quotes' → ‘quotes’
-      // it's → it’s
-      // em -- dash → em — dash
-      // en - dash → en – dash
-      // 2 x 4 → 2 × 4
-      // foo(tm) → foo™
-      // foo(r) → foo®
-      // foo(c) → foo©
-    };
-
-    // TODO: do something
-    return block;
-  }
-
-  // World's simplest lexer:
-  function lex(input) { return input.split(/\n\n+/) }
-
-  var res = input;
-  res = handle_phrase_modifiers(res);
-  res = handle_substitutions(res);
+  var res = [];
+  for(var i in doc)
+    res.push(parse_block(doc[i][0], doc[i][1]));
   return res;
 }
 
-function test() {
-  tests = ["Testing _emphasis_ and *strength*." ];
-  for(var i in tests) {
-    console.log(linen(tests[i]));
+
+function generate_code(blocks) {
+  function generate_block(block) {
+
+    function html_attrs(b) {
+      var attrs = "";
+      if(b.classes)
+        attrs += "class=\"" + b.classes + "\" ";
+
+      if(b.id)
+        attrs += "id=\"" + b.id + "\" ";
+
+      if(b.align)
+        b.style = "text-align: " + b.align + "; " + b.style
+
+      if(b.style)
+        attrs += "style=\"" + b.style + "\" ";
+
+      if(b.lang)
+        attrs += "lang=\"" + b.lang + "\" ";
+
+      return attrs;
+    }
+
+    // Generate different kinds of code blocks:
+    function heading(b) {
+      return "<" + b.block_type + " " + html_attrs(b) + ">" +
+               b.content + 
+             "</" + b.block_type + ">";
+    }
+    function blockquote(b) {
+      return "<blockquote " + html_attrs(b) + "><p>" + 
+                b.content + 
+              "</p></blockquote>";
+    }
+    function footnote(b) {
+      return "<p" + html_attrs(b) + "><sup>" + b.number + "</sup>" + 
+                b.content + 
+              "</p>"
+    }
+    function paragraph(b) {
+      return "<p " + html_attrs(b) + ">" + 
+                b.content + 
+              "</p>";
+    }
+    function blockcode(b) {
+      // TODO: Change this to fit the reference implementation,
+      // if I can think of a good reason to. 
+      return "<pre " + html_attrs(b) + "><code>" +
+               b.content + 
+             "</code></pre>";
+    }
+    function preformatted(b) {
+      return "<pre " + html_attrs(b) + ">" +
+               b.content + 
+             "</pre>";
+    }
+    function ordered_list(b) {
+      // TODO: Write this. 
+    }
+    function unordered_list(b) {
+      // TODO: Write this. 
+    }
+
+    switch(block.block_type) {
+      case "h1":
+      case "h2":
+      case "h3":
+      case "h4":
+      case "h5":
+      case "h6":
+        return heading(block);
+      case "bq":
+        return blockquote(block);
+      case "fn": // TODO: Handle footnote numbers
+        return footnote(block);
+      case "p":
+        return paragraph(block);
+      case "bc":
+        return blockcode(block);
+      case "pre":
+        return preformatted(block);
+      case "ul": // TODO: This
+        return unordered_list(block);
+      case "ol": // TODO: This too
+        return ordered_list(block);
+    }
   }
+
+  ret = "";
+  for(var i in blocks)
+    ret += generate_block(blocks[i]) + "\n";
+  return ret;
 }
-test();
+
+
+var test = "p>(funstuff){color: green}. This is a test.\n\n"+
+           "bc>(funstuff){color: green}. This is a test.\n\n"+
+           "A *simple* example.\n\n"+
+           "h2. A simple header.\n\n"+
+           "h2<>(#header)[EN]. I want to go home.\n\n"+
+           "h3<>(.test#email)(asdf)(hqzh)<[EN]{padding: 5em}(53). somebody@example.com\n\n"+
+           "h4<>(footie#gutter)(headie). Throw your trash over there, nobody will mind.\n\n"+
+           "pre{background-color: darkblue;}. I walked in the valley of the shadow of death.";
+
+console.log(generate_code(parse(lex(test))));
