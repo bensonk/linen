00ef53125351e679adc4be5231c6b61b7f7ff03b
Benson Kalahar <bensonk42@gmail.com>
2011-04-11 12:43:36 -0500

    Made cli.js read from stdin with a file of '-'

--- cli.js
+++ cli.js
@@ -3,7 +3,24 @@ var fs = require('fs');
 
 var file = process.argv[2];
 if(file) {
-  console.log(linen(fs.readFileSync(file, "ascii")));
+  if(file == '-') {
+    // Read from stdin
+    var stdin = process.openStdin();
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+
+    var data = "";
+    process.stdin.on('data', function (chunk) {
+      data = data + chunk;
+    });
+    process.stdin.on('end', function() {
+      console.log(linen(data));
+    });
+  }
+  else {
+    // Read from a file
+    console.log(linen(fs.readFileSync(file, "ascii")));
+  }
 }
 else {
   console.log("Usage: node cli.js <file.textile>");
